a1870d90c918570c7cc67ff25bf79cef359aaeb3
Marcos Gabarda <hey@marcosgabarda.com>
2022-05-06 15:53:29 +0200

    feat: refactor of code

---
+++ game/app.py
@@ -0,0 +1,60 @@
+import pyxel
+
+from game.world import World
+
+
+class App:
+    """Default pyxel app."""
+
+    def __init__(self, screen_width: int, screen_height: int, block_size: int) -> None:
+        # Save initial data
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.block_size = block_size
+
+        # Creates the world
+        self.world = World(
+            self.screen_width // self.block_size, self.screen_height // self.block_size
+        )
+
+        # Launch pyxel
+        pyxel.init(self.screen_width, self.screen_height, title="Zero Players Game")
+        pyxel.playm(0, loop=True)
+        pyxel.run(self.update_world, self.draw_world)
+
+    def _color(self, value: float) -> int:
+        """Gets the color for a given value."""
+        if value < 0.3:
+            return 5
+        elif value < 0.5:
+            return 11
+        elif value < 0.8:
+            return 3
+        return 7
+
+    def _step(self) -> None:
+        """Make the movement."""
+        self.world.move_down()
+
+    def update_world(self):
+        """Updates the status of the world."""
+        # Handles the quit event.
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+        # Movement
+        self._step()
+
+    def draw_world(self):
+        """Draws the current view of the world."""
+        pyxel.cls(12)
+        chunk = self.world.chunk()
+        for x in range(chunk.shape[0]):
+            for y in range(chunk.shape[1]):
+                color = self._color(value=chunk[x, y])
+                pyxel.rect(
+                    x * self.block_size,
+                    y * self.block_size,
+                    self.block_size,
+                    self.block_size,
+                    color,
+                )
---
+++ game/world.py
@@ -0,0 +1,59 @@
+from typing import Optional
+
+import numpy as np
+
+
+class World:
+    """Class that represents the generation of the world."""
+
+    SPEED: int = 1
+
+    def __init__(self, width: int, height: int, speed: Optional[int] = None):
+        """Initializes the tile with the width and height given for the chunk."""
+        self.width: int = width
+        self.height: int = height
+        self.speed: int = speed if speed is not None else self.SPEED
+        self.tile: "np.ndarray" = self._generate_tile(
+            width * 3, height * 3
+        )  # Generates a tile with the current chunk and the nearest chunks
+
+    def _generate_tile(self, width, height) -> "np.ndarray":
+        """Generates a complete tile."""
+        return np.random.rand(width, height)
+
+    def _generate_tile_increment(self, axis: int = 0) -> "np.ndarray":
+        """Generates an array with the increment for the tile."""
+        if axis == 0:
+            return np.random.rand(self.SPEED, self.tile.shape[1])
+        elif axis == 1:
+            return np.random.rand(self.tile.shape[0], self.SPEED)
+        else:
+            raise Exception("The value of axis have to be 0 or 1")
+
+    def chunk(self):
+        """Gets the central chunk."""
+        return self.tile[self.width : -self.width, self.height : -self.height]
+
+    def move_left(self):
+        """Moves the tiles to the left."""
+        self.tile = np.concatenate((self._generate_tile_increment(), self.tile))[
+            : -self.SPEED
+        ]
+
+    def move_right(self):
+        """Moves the tiles to the right."""
+        self.tile = np.concatenate((self.tile, self._generate_tile_increment()))[
+            self.SPEED :
+        ]
+
+    def move_up(self):
+        """Moves the tiles to the up."""
+        self.tile = np.concatenate(
+            (self._generate_tile_increment(axis=1), self.tile), axis=1
+        )[:, : -self.SPEED]
+
+    def move_down(self):
+        """Moves the tiles to the down."""
+        self.tile = np.concatenate(
+            (self.tile, self._generate_tile_increment(axis=1)), axis=1
+        )[:, self.SPEED :]
