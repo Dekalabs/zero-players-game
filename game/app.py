d0f0209b71affe5eddc70e757e58a42cbc8bd08d
Francisco Javier Martínez Talavera <fcojavierdev@gmail.com>
2022-05-09 12:13:11 +0200

    feat(biome): added biome file

--- game/app.py
+++ game/app.py
@@ -1,6 +1,7 @@
 import pyxel
 
 from game.world import World
+from game.biome import Biome
 
 
 class App:
@@ -18,21 +19,14 @@ class App:
             self.screen_width // self.block_size, self.screen_height // self.block_size
         )
 
+        # Creates the Biome
+        self.biome = Biome(self.world, self.block_size)
+
         # Launch pyxel
         pyxel.init(self.screen_width, self.screen_height, title="Zero Players Game")
         pyxel.playm(0, loop=True)
         pyxel.load('../assets/resources.pyxres')
-        pyxel.run(self.update_world, self.draw_world)
-
-    def _color(self, value: float) -> int:
-        """Gets the color for a given value."""
-        if value < -0.4:
-            return 5
-        elif value < 0:
-            return 11
-        elif value < 0.4:
-            return 3
-        return 7
+        pyxel.run(self.update_world, self.biome.draw_world)
 
     def _step(self) -> None:
         """Make the movement."""
@@ -57,21 +51,6 @@ class App:
 
         # Music
         self.music_controls()
-
-    def draw_world(self):
-        """Draws the current view of the world."""
-        pyxel.cls(12)
-        chunk = self.world.chunk()
-        for x in range(chunk.shape[0]):
-            for y in range(chunk.shape[1]):
-                color = self._color(value=chunk[x, y])
-                pyxel.rect(
-                    x * self.block_size,
-                    y * self.block_size,
-                    self.block_size,
-                    self.block_size,
-                    color,
-                )
 
     def music_controls(self):
         if pyxel.btnp(pyxel.KEY_M):
